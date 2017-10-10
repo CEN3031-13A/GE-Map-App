8deef5cddc928a93f201c64b663e778b90ea8b16
Kassandra Crompton <kcrompton@ufl.edu>
2017-10-10 19:48:35 -0400

    predix-webapp-starter, original side navigation

---
+++ predix-webapp-starter/public/bower_components/px-key-value-pair/dist/px-key-value-pair.js
@@ -0,0 +1,2 @@
+'use strict';(function(){Polymer({is:'px-key-value-pair',properties:{counterValue:{type:Number,value:0,notify:true}},handleClick:function handleClick(event,detail,sender){this.increment()},increment:function increment(){this.counterValue++}})})();
+//# sourceMappingURL=px-key-value-pair.js.map
